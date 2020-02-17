865316a7227ed9e32a0c01c9c554c224a1f8c3b8
ysp五天 <16655636335@163.com>
2020-02-18 00:39:55 +0800

    day02

---
+++ gmall-user-service/src/main/java/com/gmall/userservice/GmallUserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.gmall.userservice;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.gmall.userservice.mapper")
+public class GmallUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserServiceApplication.class, args);
+    }
+
+}
